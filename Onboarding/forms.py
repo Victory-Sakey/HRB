7c0e0b35a260e8a59f58e6a8b51da6b35060b908
Victory-Sakey <victorysakey66@gmail.com>
2024-07-28 19:23:37 -0700

    Added

--- Onboarding/forms.py
+++ Onboarding/forms.py
@@ -8,11 +8,11 @@ class ContactForm(forms.ModelForm):
         fields = ['name' , 'email', 'number', 'message']
         widgets = {
             'name': forms.TextInput(attrs={
-                'class': 'w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'}),
+                'class': 'w-full bg-gray-300 text-gray-900 mt-2 p-1 focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'}),
             'email': forms.EmailInput(attrs={
-                'class': 'w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'}),
+                'class': 'w-full bg-gray-300 text-gray-900 mt-2 p-1 focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'}),
             'number': forms.TextInput(attrs={
-                'class': 'w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'}),
+                'class': 'w-full bg-gray-300 text-gray-900 mt-2 p-1 focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'}),
             'message': forms.Textarea(attrs={
-                'class': 'w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'})
+                'class': 'w-full h-28 bg-gray-300 text-gray-900 mt-2 p-1  focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-green-400'})
         }
